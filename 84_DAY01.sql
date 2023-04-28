a2dde80e64e07ff9913eabe8cba1638633e6522d
Serdal Özçetin <ozcetinserdal@gmail.com>
2023-04-28 07:57:49 +0300

    28.04.2023

--- 84_DAY01.sql
+++ 84_DAY01.sql
@@ -79,11 +79,44 @@ student_id SMALLINT,
 student_name VARCHAR(50),
 student_age SMALlINT,
 student_dob DATE,
-CONSTRAINT student_age_check CHECK(student_age BETWEEN 0 AND 20),--0 NAD 20 INCLUDING
+CONSTRAINT student_age_check CHECK(student_age BETWEEN 0 AND 20),  -- 0 NAD 20 INCLUDING
 CONSTRAINT student_name_check CHECK(student_name = UPPER(student_name) )
-	
 );
 
+
+
+--how to add ınfo ınto the table
+CREATE TABLE students
+(
+student_id SMALLINT PRIMARY KEY,
+student_name VARCHAR(50) UNIQUE,
+student_age SMALlINT NOT NULL,
+student_dob DATE,
+CONSTRAINT student_age_check CHECK(student_age BETWEEN 0 AND 20),  -- 0 NAD 20 INCLUDING
+CONSTRAINT student_name_check CHECK(student_name = UPPER(student_name) )
+);
+
+INSERT INTO students VALUES(101,'ALİ',18,'12.Dec.2000');
+INSERT INTO students VALUES(102,'MEHMET',18,'12.Nov.2000');
+INSERT INTO students VALUES(103,NULL,20,'12.Jul.2000');
+
+--integer values can be used with single quote or without single quote
+INSERT INTO students VALUES(105,'SEMİH',20,NULL);
+INSERT INTO students VALUES('104','KENAN',20,'12.Jul.2000');
+INSERT INTO students VALUES('111','BENJAMIN',20,'11.03.2002');
+
+--HOW TO INSERT INFO INTO ANY SPECİFİC COLUMN
+INSERT INTO students(student_id,student_age) VALUES(112,14);
+INSERT INTO students(student_name,student_id,student_age) VALUES('JHON',119,14);
+
+--HOW TO UPDATE THE EXİSTİNG DATA
+UPDATE students
+SET student_name = 'LEO'
+WHERE student_id = 112;
+
+UPDATE students
+SET student_dob = '10.08.2017'
+WHERE student_id = 105;
 
 
 
