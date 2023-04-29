0cd8193a8df40a19eddfc767c7578ef827114098
Serdal Özçetin <ozcetinserdal@gmail.com>
2023-04-29 13:50:24 +0300

    29.04.2023

--- 84_DAY02.sql
+++ 84_DAY02.sql
@@ -71,9 +71,13 @@ SELECT MIN(worker_salary) AS third_lowest_salary
 FROM workers
 WHERE worker_salary > (SELECT MIN(worker_salary)
                        FROM workers
-                       WHERE worker_salary > (SELECT MIN(worker_salary) FROM workers);
-					   
---QUERY RECORDS WITH THE 2. HIGEST SALARY
+                       WHERE worker_salary > (SELECT MIN(worker_salary) FROM workers));
+					   SELECT MIN(worker_salary) AS third_lowest_salary
+FROM workers
+WHERE worker_salary > (SELECT MIN(worker_salary)
+                       FROM workers
+                       WHERE worker_salary > (SELECT MIN(worker_salary) FROM workers));
+--QUERY RECORD WITH THE 2. HIGEST SALARY
 --1.Way
 SELECT *
 FROM workers
@@ -86,3 +90,128 @@ FROM workers
 ORDER BY worker_salary DESC
 OFFSET 1 ROW
 FETCH NEXT 1 ROW ONLY;
+					   
+					   
+					   
+--QUERY RECORD WITH THE 2. LOWEST SALARY
+SELECT *
+FROM workers
+WHERE worker_salary = (SELECT MIN(worker_salary)
+					   FROM workers
+					   WHERE worker_salary > (SELECT MIN(worker_salary) FROM workers));
+					   
+					   
+					   
+
+SELECT *
+FROM workers
+ORDER BY worker_salary ASC
+OFFSET 1 ROW
+FETCH NEXT 1 ROW ONLY;
+					   
+					   
+--QUERY RECORD WITH THE 3. HIGHEST SALARY
+SELECT *
+FROM workers
+WHERE worker_salary = 	(SELECT MAX(worker_salary)
+						 FROM workers
+						 WHERE worker_salary < (SELECT MAX(worker_salary)
+                       							FROM workers
+                      							WHERE worker_salary < (SELECT MAX(worker_salary) FROM workers)));
+												
+												
+--2.WAY
+SELECT *
+FROM workers
+ORDER BY worker_salary DESC
+OFFSET 3 ROW
+FETCH NEXT 1 ROW ONLY;
+------------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE customers_products
+(
+product_id CHAR(10),
+customer_name VARCHAR(50),
+product_name VARCHAR(50)
+);
+
+INSERT INTO customers_products VALUES (10, 'Mark', 'Orange');
+INSERT INTO customers_products VALUES (10, 'Mark', 'Orange');
+INSERT INTO customers_products VALUES (10, 'Jhon', 'Apple');
+INSERT INTO customers_products VALUES (30, 'Amy', 'Palm');
+INSERT INTO customers_products VALUES (20, 'Mark', 'Apple');
+INSERT INTO customers_products VALUES (10, 'Adem', 'Orange');
+INSERT INTO customers_products VALUES (40, 'Jhon', 'Apricot');
+INSERT INTO customers_products VALUES (20, 'Eddie', 'Apple');
+
+--QUERY RECORDS WITH THE PRODUCTS_NAME "ORANGE" OR "APPLE" OR "PALM"
+
+SELECT*
+FROM customers_products
+WHERE product_name IN('Orange','Apple','Palm');
+
+--QUERY RECORDS WITHOUT THE PRODUCTS_NAME "ORANGE" OR "APPLE" OR "PALM"
+SELECT*
+FROM customers_products
+WHERE product_name NOT IN ('Orange','Apple','Palm');
+
+--BETWEEN CONDITION
+--QUERY THE RECORDS WITH PRODUCTS İD >= 20 OR <= 30
+
+--1. WAY
+SELECT*
+FROM customers_products
+WHERE product_id >= '20' AND product_id <= '30';
+
+
+SELECT*
+FROM customers_products
+WHERE product_id BETWEEN '20' AND '30';
+
+
+--BETWEEN CONDITION
+--QUERY THE RECORDS WITHOUT PRODUCTS İD >= 20 OR <= 30
+
+--2. WAY
+SELECT*
+FROM customers_products
+WHERE product_id NOT BETWEEN '20' AND '30';
+
+
+CREATE TABLE customers_likes
+(
+product_id CHAR(10),
+customer_name VARCHAR(50),
+liked_products VARCHAR(50)
+);
+
+INSERT INTO customers_likes VALUES (10, 'Lary', 'Orange');
+INSERT INTO customers_likes VALUES (10, 'Lary', 'Orange');
+INSERT INTO customers_likes VALUES (10, 'Jhon', 'Apple');
+INSERT INTO customers_likes VALUES (30, 'Amy', 'Palm');
+INSERT INTO customers_likes VALUES (20, 'Mark', 'Apple');
+INSERT INTO customers_likes VALUES (10, 'Lary', 'Orange');
+INSERT INTO customers_likes VALUES (40, 'Jhon', 'Apricot');
+INSERT INTO customers_likes VALUES (20, 'Eddie', 'Apple');
+
+
+
+--UPDATE THE RECORDS NAME AS "NO NAME" IF "LARY" EXISTS IN THE PRODUCT_NAME
+
+UPDATE customers_likes
+SET customer_name = 'No Name'
+WHERE EXISTS (SELECT product_id FROM customers_likes WHERE customer_name ='Lary');
+
+
+
+
+
+
+
+
+
+SELECT*
+FROM customers_likes;
+
+
+
+
